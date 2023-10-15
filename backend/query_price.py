addb4e7ae2710e846a2d779f536e0fdb07e3247e
Justin <j.tann2305@gmail.com>
2023-10-15 15:30:25 +0800

    added API query

---
+++ backend/query_price.py
@@ -0,0 +1,39 @@
+import os 
+import requests #Allows sending of HTTP requests to URL 
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+URL = 'https://api.currencyapi.com/v3/latest'
+
+TERMPAIRS = ["BND","CNY","HKD","IDR","INR",
+            "JPY","KRW","LKR","MYR", "PHP", 
+            "SGD","THB","VND"]
+
+params = { #params vs headers 
+    'apikey' : os.environ.get('CURRENCY_API_KEY')
+}
+
+
+def fetch_rates():
+    '''
+    Fetches the current rates from API in the form of USDXXX
+    
+    :return: JSON of rates 
+    :rtype: Dictionary of some rates
+    '''
+    response = requests.get(URL, params=params)
+    data = response.json()
+    
+    prices = {} #CountryCode:value
+    for term, term_data in data['data'].items(): 
+        if term in TERMPAIRS:
+            prices[term] = term_data['value']
+    print(prices)
+    
+    
+    
+
+    
+if __name__ == '__main__':
+    fetch_rates()
+    
