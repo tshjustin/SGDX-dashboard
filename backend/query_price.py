3f5356f9a74e8507cdd2f8aaab08935e80fd1483
Justin <j.tann2305@gmail.com>
2023-10-16 23:14:56 +0800

    refactored to allow for rates conversion

--- backend/query_price.py
+++ backend/query_price.py
@@ -2,10 +2,11 @@ import os
 import requests #Allows sending of HTTP requests to URL 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+from utils import get_sgd_rates, sgd_to_term
 
 URL = 'https://api.currencyapi.com/v3/latest'
 
-TERMPAIRS = ["BND","CNY","HKD","IDR","INR",
+BASE_TERM_PAIRS = ["BND","CNY","HKD","IDR","INR",
             "JPY","KRW","LKR","MYR", "PHP", 
             "SGD","THB","VND"]
 
@@ -15,19 +16,22 @@ params = { #params vs headers
 
 def fetch_rates():
     '''
-    Fetches the current rates from API in the form of USDXXX
+    Fetches the current rates from API in the form of USDXXX 
     
     :return: JSON of rates 
-    :rtype: Dictionary of some rates
+    :rtype: Dictionary - With key:value pairs of base:value
     '''
     response = requests.get(URL, params=params)
     data = response.json()
     
     prices = {} #CountryCode:value
     for term, term_rate in data['data'].items(): 
-        if term in TERMPAIRS:
+        if term in BASE_TERM_PAIRS:
             prices[term] = term_rate['value']
-        return prices
- 
+    return prices
+
 if __name__ == '__main__':
-    fetch_rates()
+    prices = fetch_rates()
+    sgd_rate = get_sgd_rates(prices)
+    term = sgd_to_term(sgd_rate,30)
+    print(term)
