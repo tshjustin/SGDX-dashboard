c81a54dbe6e5bdec70bafe8e4d9fbbbfb045b805
Justin <j.tann2305@gmail.com>
2023-10-17 10:34:32 +0800

    refactored main

--- backend/query_price.py
+++ backend/query_price.py
@@ -2,13 +2,14 @@ import os
 import requests #Allows sending of HTTP requests to URL 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
-from utils import get_sgd_rates, sgd_to_term
-
+from database_handler import insert_rates
+    
 URL = 'https://api.currencyapi.com/v3/latest'
 
-BASE_TERM_PAIRS = ["BND","CNY","HKD","IDR","INR",
-            "JPY","KRW","LKR","MYR", "PHP", 
-            "SGD","THB","VND"]
+BASE_TERM_PAIRS = ["BND","CNY","HKD","IDR",
+                   "INR","JPY","KRW","LKR",
+                   "MYR", "PHP", "SGD","THB",
+                   "VND"]
 
 params = { #params vs headers 
     'apikey' : os.environ.get('CURRENCY_API_KEY')
@@ -32,6 +33,5 @@ def fetch_rates():
 
 if __name__ == '__main__':
     prices = fetch_rates()
-    sgd_rate = get_sgd_rates(prices)
-    term = sgd_to_term(sgd_rate,30)
-    print(term)
+    insert_rates(prices)
+    #print(prices)
